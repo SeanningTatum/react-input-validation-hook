64a70fbd7618a558fd9ae1e39ec0f888f927ba19
Sean Urgel <seantheurgel@gmail.com>
2019-03-27 10:32:17 +0800

    build for github pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import useFormValidation from "./lib";
+// Import package
+import React from 'react'
+import {render} from 'react-dom'
+import useFormValidation from './lib'
 
-export default useFormValidation
+function App() {
+    // Any of these can be renamed ex: [fnErrors, firstName, isFnValid] = ...
+    const [errors, myValue, isValid] = useFormValidation('init val', {minLength: 6, maxLength: 8, required: true})
 
+    return (
+        <div>
+            <label htmlFor="">Try typing here</label>
+            {/* Use spread operator here, because onChange, onBlur and value is handled for you. */}
+            <input type="text" {...myValue}/>
+            <p>{isValid ? 'Value is Valid' : 'Value is Invalid'}</p>
+            {/* Print out errors */}
+            {errors.map(e => <p key={e} style={{color: 'red'}}>{e}</p>)}
+      </div>
+    )
+}
 
+render(<App />, document.getElementById("root"));
